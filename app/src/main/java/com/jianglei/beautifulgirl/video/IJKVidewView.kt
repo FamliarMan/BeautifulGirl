ff3b01f2f3b8a0027e6b1875b3b4cdd1ae0ea510
jianglei <2413542518@qq.com>
2019-01-08 22:53:51 +0800

    搞定视频播放

---
+++ app/src/main/java/com/jianglei/beautifulgirl/video/IJKVidewView.kt
@@ -0,0 +1,7 @@
+package com.jianglei.beautifulgirl.video
+
+/**
+ * @author jianglei on 1/8/19.
+ */
+class IJKVidewView {
+}
--- app/src/main/java/com/jianglei/beautifulgirl/video/VideoPlayActivity.kt
+++ app/src/main/java/com/jianglei/beautifulgirl/video/VideoPlayActivity.kt
@@ -1,75 +1,90 @@
 package com.jianglei.beautifulgirl.video
 
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import android.view.Window
 import android.view.WindowManager
+import com.dl7.player.media.IjkPlayerView
 import com.jianglei.beautifulgirl.R
-import io.vov.vitamio.LibsChecker
-import io.vov.vitamio.MediaPlayer
-import io.vov.vitamio.Vitamio
-import io.vov.vitamio.widget.MediaController
 import kotlinx.android.synthetic.main.activity_video_play.*
 
 class VideoPlayActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //定义全屏参数
-        val flag = WindowManager.LayoutParams.FLAG_FULLSCREEN
-        //获得当前窗体对象
-        val window = window
-        //设置当前窗体为全屏显示
-        window.setFlags(flag, flag)
-        //必须写这个，初始化加载库文件
-        Vitamio.initialize(this)
-        //设置视频解码监听
-        if (!LibsChecker.checkVitamioLibs(this)) {
-            return
-        }
 
         setContentView(R.layout.activity_video_play)
         init()
-        playContent.start()
     }
 
-    fun init(){
-        playContent.setVideoURI(Uri.parse("http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"))
-        playContent.setMediaController(MediaController(this))
-        playContent.setVideoQuality(MediaPlayer.VIDEOQUALITY_HIGH)
-        playContent.requestFocus()
-        playContent.setOnPreparedListener{
-            it.setPlaybackSpeed(1.0F)
-        }
-        playContent.setOnInfoListener(onInfoListener)
-        playContent.setOnBufferingUpdateListener(onBufferingUpdateListener)
+    fun init() {
+        playContent.init()
+            .setVideoPath(Uri.parse("http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"))
+            .setMediaQuality(IjkPlayerView.MEDIA_QUALITY_HIGH)
+            .start()
     }
 
-    private val onInfoListener = MediaPlayer.OnInfoListener { mp, what, extra ->
-//        when{
-//            what == MediaPlayer.MEDIA_INFO_BUFFERING_START ->{
-//
-//            }
-//            what == MediaPlayer.MEDIA_INFO_BUFFERING_START ->{
-//
-//            }
-//            what == MediaPlayer.MEDIA_INFO_BUFFERING_START ->{
-//
-//            }else {
-//        }
-//
-//        }
-//
-//
-//        }
-        true
+    override fun onResume() {
+        super.onResume()
+        playContent.onResume()
     }
-    private val onBufferingUpdateListener = MediaPlayer.OnBufferingUpdateListener { mp, percent ->
+
+    override fun onPause() {
+        super.onPause()
+        playContent.onPause()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        playContent.stopPlayback()
+        playContent.onDestroy()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        playContent.configurationChanged(newConfig)
+//        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE ) {//横屏
+////            val flag = WindowManager.LayoutParams.FLAG_FULLSCREEN
+////            val window = window
+////            window.setFlags(flag, flag)
+//            supportActionBar!!.hide()
+//        }else if(resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT){
+//            supportActionBar!!.show()
+//        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (playContent.handleVolumeKey(keyCode)) {
+            return true
+        }
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            // 检测屏幕的方向：纵向或横向
+            if (this.resources.configuration.orientation
+                == Configuration.ORIENTATION_LANDSCAPE
+            ) {
+                //当前为横屏，切换至竖屏
+                this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+
+            } else if (this.resources.configuration.orientation
+                == Configuration.ORIENTATION_PORTRAIT
+            ) {
+                //当前为竖屏，按退出键后就结束当前activity
+                finish()
+            }
+
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onBackPressed() {
+        if (playContent.onBackPressed()) {
+            return
+        }
+        super.onBackPressed()
     }
 
 }
