ed47f16bc93bc243c2f1b9c7787af9be6aa55d38
jianglei <2413542518@qq.com>
2019-01-26 20:33:41 +0800

    增加photoView使用

---
+++ app/src/main/java/com/jianglei/beautifulgirl/PhotoViewActivity.kt
@@ -0,0 +1,16 @@
+package com.jianglei.beautifulgirl
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_photo_view.*
+
+class PhotoViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_view)
+        val url = intent.getStringExtra("url")
+        photoView.setPhotoUri(Uri.parse(url))
+    }
+}
