71c1b04af9ffe2dc54c6b95564aabee88d8bfd90
jianglei <2413542518@qq.com>
2019-03-30 14:56:44 +0800

    删除不需要的类

--- app/src/main/java/com/jianglei/beautifulgirl/data/OnItemClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.jianglei.beautifulgirl.data
-
-/**
- * @author jianglei on 1/11/19.
- */
-interface OnItemClickListener<T> {
-    fun onItemClick(item:T,pos:Int);
-}
--- app/src/main/java/com/jianglei/beautifulgirl/view/DocumentEditView.kt
+++
@@ -1,7 +0,0 @@
-package com.jianglei.beautifulgirl.view
-
-/**
- * @author jianglei on 3/24/19.
- */
-class DocumentEditView {
-}
